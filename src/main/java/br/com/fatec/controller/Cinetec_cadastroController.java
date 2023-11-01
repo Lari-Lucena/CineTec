f14807218b807de90c73ee20e4c567791a28f0c5
gmyos <gmayse@hotmail.com>
2023-11-01 11:07:29 -0300

    Criação telas ingressos

--- src/main/java/br/com/fatec/controller/Cinetec_cadastroController.java
+++ src/main/java/br/com/fatec/controller/Cinetec_cadastroController.java
@@ -6,7 +6,13 @@ package br.com.fatec.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -14,6 +20,29 @@ import javafx.fxml.Initializable;
  * @author Leonardo
  */
 public class Cinetec_cadastroController implements Initializable {
+
+    @FXML
+    private Button btn_cadastrar;
+    @FXML
+    private TextField txt_nome;
+    @FXML
+    private TextField txt_apelido;
+    @FXML
+    private TextField txt_cpf;
+    @FXML
+    private TextField txt_celular;
+    @FXML
+    private TextField txt_birth;
+    @FXML
+    private ComboBox<?> cmb_regiao;
+    @FXML
+    private TextField txt_email;
+    @FXML
+    private PasswordField pwd_senha;
+    @FXML
+    private PasswordField pwd_confirmar;
+    @FXML
+    private CheckBox chk_visualizar;
 
     /**
      * Initializes the controller class.
