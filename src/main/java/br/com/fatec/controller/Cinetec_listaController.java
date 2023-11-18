ba0fadc12d849b4b37254c8fa245f172ce0d9506
gmyos <gmayse@hotmail.com>
2023-11-17 22:50:35 -0300

    table view

--- src/main/java/br/com/fatec/controller/Cinetec_listaController.java
+++ src/main/java/br/com/fatec/controller/Cinetec_listaController.java
@@ -1,13 +1,78 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package br.com.fatec.controller;
 
+import br.com.fatec.model.CadFilmes;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SortEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 /**
+ * FXML Controller class
  *
- * @author Larica
+ * @author Leonardo
  */
-public class Cinetec_listaController {
+public class Cinetec_listaController implements Initializable {
+
+    @FXML
+    private TableView<?> tbLista;
+    @FXML
+    private TableColumn<?, ?> colID;
+    @FXML
+    private TableColumn<?, ?> colNome;
+    @FXML
+    private TableColumn<?, ?> colGenero;
+    @FXML
+    private TableColumn<?, ?> colClassificacao;
+    @FXML
+    private TableColumn<?, ?> colSinopse;
+    @FXML
+    private TableColumn<?, ?> colDistribuidora;
+    @FXML
+    private TableColumn<?, ?> colImagem;
+    @FXML
+    private TableColumn<?, ?> colDistribuidora1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void colID(TableColumn.CellEditEvent<CadFilmes, T> event) {
+    }
+
+    @FXML
+    private void colNome(TableColumn.CellEditEvent<S, T> event) {
+    }
+
+    @FXML
+    private void colGenero(TableColumn.CellEditEvent<S, T> event) {
+    }
+
+    @FXML
+    private void colClassificacao(TableColumn.CellEditEvent<S, T> event) {
+    }
+
+    @FXML
+    private void colSinopse(TableColumn.CellEditEvent<S, T> event) {
+    }
+
+    @FXML
+    private void colImagem(TableColumn.CellEditEvent<S, T> event) {
+    }
+
+    @FXML
+    private void tbLista(SortEvent<CadFilmes> event) {
+    }
     
 }
